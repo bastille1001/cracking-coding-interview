ca67116f14f4eddd5885ec09456cc13d0ecf6e27
bastille1001 <timur.guseynovv@gmail.com>
2025-01-22 21:58:10 +0400

    add initial coding

---
+++ Cracking.Coding.Interview/ArraysAndStrings.cs
@@ -0,0 +1,113 @@
+﻿namespace Cracking.Coding.Interview
+{
+    public static class ArraysAndStrings
+    {
+        /// <summary>
+        /// Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public static bool IsUnique(string word)
+        {
+            var hs = word.ToHashSet();
+
+            return word.Length == hs.Count;
+        }
+
+        /// <summary>
+        /// Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public static bool IsUnique2(string word)
+        {
+            var charArr = word.ToCharArray();
+            Array.Sort(charArr);
+
+            for (int i = 1; i < charArr.Length; i++)
+            {
+                if (charArr[i] == charArr[i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // <summary>
+        /// Check Permutation: Given two strings, write a method to decide if one is a permutation of the other.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public static bool CheckPermutation(string original, string permutated)
+        {
+            if (original.Length != permutated.Length)
+            {
+                return false;
+            }
+
+            var dict = new Dictionary<char, int>();
+
+            foreach (char ch in original)
+            {
+                if (dict.TryGetValue(ch, out int value))
+                {
+                    dict[ch] = ++value;
+                }
+                else
+                {
+                    dict.Add(ch, ++value);
+                }
+            }
+
+            foreach (var perm in permutated)
+            {
+                if (dict.TryGetValue(perm, out int value) && !(value <= 0))
+                {
+                    dict[perm] = --value;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // <summary>
+        /// Check Permutation: Given two strings, write a method to decide if one is a permutation of the other.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public static bool CheckPermutation2(string original, string permutated)
+        {
+            if (original.Length != permutated.Length)
+            {
+                return false;
+            }
+
+            var originCharArr = original.ToCharArray();
+            var permCharArr = permutated.ToCharArray();
+
+            Array.Sort(originCharArr);
+            Array.Sort(permCharArr);
+
+            for (int i = 0; i < originCharArr.Length; i++)
+            {
+                if (originCharArr[i] != permCharArr[i]) 
+                { 
+                    return false; 
+                }
+            }
+
+            return true;
+        }
+
+        public static string URLify(string input)
+        {
+            return input.TrimEnd().Replace(" ", "%20");
+        }
+    }
+}
---
+++ Cracking.Coding.Interview/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Cracking.Coding.Interview
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine($"{nameof(ArraysAndStrings.IsUnique)} returned {ArraysAndStrings.IsUnique("teymure")}");   // IsUnique - Arrays and Strings with HashSet
+            Console.WriteLine($"{nameof(ArraysAndStrings.IsUnique2)} returned {ArraysAndStrings.IsUnique2("teymure")}"); // IsUnique - Arrays and Strings without additional data structure
+
+
+
+            Console.WriteLine($"{nameof(ArraysAndStrings.CheckPermutation)} returned {ArraysAndStrings.CheckPermutation("teymur", "rteym")}");  // Check Permutation: Given two strings, write a method to decide if one is a permutation of the other with Dictionary
+            Console.WriteLine($"{nameof(ArraysAndStrings.CheckPermutation2)} returned {ArraysAndStrings.CheckPermutation2("teymur", "rteym")}"); // Check Permutation: Given two strings, write a method to decide if one is a permutation of the other without additional data structure
+
+
+
+            Console.WriteLine(ArraysAndStrings.URLify("Mr John Smith   "));
+        }
+    }
+}
